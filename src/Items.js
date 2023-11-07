95d955a7d3203ec816851bf9c5d2ccbecb89c79c
Guram12 <guram.shanidze.33@gmail.com>
2023-11-07 22:01:58 +0400

    fixted manifest error

--- src/Items.js
+++ src/Items.js
@@ -1,80 +1,56 @@
-import './Items.css'
+import React, { useState, useEffect } from 'react';
 
+export default function ItemList() {
+  const [items, setItems] = useState([]);
+  const [loading, setLoading] = useState(true);
 
-import React, { useState } from 'react';
+  useEffect(() => {
+    async function fetchItems() {
+      try {
+        const token = localStorage.getItem('authToken');
+        const headers = {
+          'Content-Type': 'application/json',
+          'Authorization': `Token ${token}`,
+        };
+        const response = await fetch('http://127.0.0.1:8000/items/', {
+          method: 'GET',
+          headers: headers,
+        });
 
-export default function Items() {
-  const [itemData, setItemData] = useState({
-    title: '',
-    description: '',
-    price: '',
-    specifications: false,
-    mobile: '',
-    category: 1, // Set the appropriate category ID
-    image: null, // If you want to handle file uploads, use 'File' type
-  });
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
 
-  const handleInputChange = (e) => {
-    const { name, value } = e.target;
-    setItemData({
-      ...itemData,
-      [name]: value,
-    });
-  };
-
-  const handleFileUpload = (e) => {
-    const file = e.target.files[0];
-    setItemData({
-      ...itemData,
-      image: file,
-    });
-  };
-
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-
-    try {
-      const formData = new FormData();
-      for (const key in itemData) {
-        formData.append(key, itemData[key]);
+        const data = await response.json();
+        console.log(data)
+        setItems(data);
+        setLoading(false);
+      } catch (error) {
+        console.error('Error fetching items:', error);
+        setLoading(false);
       }
-      const token = localStorage.getItem("authToken")
-      const response = await fetch('http://127.0.0.1:8000/items/', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-            'Authorization': `Token ${token}`,
-        },
-        body: formData,
-      });
-
-      if (response.ok) {
-        const newItem = await response.json();
-        // Update your state or perform any necessary action with the new item data
-      } else {
-        console.error('Error creating item');
-      }
-    } catch (error) {
-      console.error('Error creating item:', error);
     }
-  };
 
-  
+    fetchItems();
+  }, []);
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
   return (
     <div>
-      <h1>Create Item</h1>
-      <form onSubmit={handleSubmit}>
-        <div>
-          <label>Title:</label>
-          <input type="text" name="title" value={itemData.title} onChange={handleInputChange} />
-        </div>
-        {/* Add similar input fields for other item details */}
-        <div>
-          <label>Image:</label>
-          <input type="file" name="image" onChange={handleFileUpload} />
-        </div>
-        <button type="submit">Create Item</button>
-      </form>
+      <h1>Item List</h1>
+      <ul>
+        {items.map((item) => (
+          <li key={item.id}>
+            <h2>{item.title}</h2>
+            <p>{item.description}</p>
+            <p>Price: {item.price}</p>
+            {/* Display other item details as needed */}
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
